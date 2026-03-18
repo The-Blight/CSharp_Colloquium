7c824bf5c1434707a5076f04ebcb1132378fc992
Khezer <hezer.fil@gmail.com>
2026-03-18 15:48:54 +0300

    Сделал документацию для интерфейса IRepository и написал пару методов для Warehouse

--- ElectronicRepairShop.Core/Entities/Warehouse/Warehouse.cs
+++ ElectronicRepairShop.Core/Entities/Warehouse/Warehouse.cs
@@ -5,8 +5,25 @@ namespace ElectronicRepairShop.Core.Entities.Warehouse;
 
 public class Warehouse
 {
-    private readonly Dictionary<Guid, Warehouse> _inventory = new(); 
-    
+    private readonly Dictionary<string, WarehouseItem> _inventory = new();
+
+    public void AddStock(WarehouseItem item)
+    {
+        if (_inventory.TryGetValue(item.Part.Article, out var existingItem))
+        {
+            existingItem.QuantityInStock += item.QuantityInStock;
+            return;
+        }
+
+        _inventory.Add(item.Part.Article, item);
+    }
+
+
+    public WarehouseItem? GetItem(string article)
+    {
+        _inventory.TryGetValue(article, out var item);
+        return item; 
+    }
     
     
 }
--- ElectronicRepairShop.Core/Interfaces/IRepository.cs
+++ ElectronicRepairShop.Core/Interfaces/IRepository.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 
 namespace ElectronicRepairShop.Core.Interfaces;
 
+/// <summary>
+/// Базовый интерфейс для всех репозиториев системы
+/// </summary>
+/// <typeparam name="T"> тип сущности с которой работает репозиторий</typeparam>
 public interface IRepository<T> where T : class
 {
     T? GetById(Guid id);
